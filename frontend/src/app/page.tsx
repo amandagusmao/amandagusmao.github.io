bdef3184c0889a7e87170fbe76980d7c227b3d6d
Amanda Gusmão <engamandagusmao@gmail.com>
2024-09-12 21:18:42 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { blackFridayFlag } from '@/app/flags';
+import { blackFridayFlag } from './app/flags';
 export default async function Page() {
   const black_friday = await blackFridayFlag();
   return (
